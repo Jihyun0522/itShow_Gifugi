585620052f5c917365f95085374bf909728b0631
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-04-15 21:48:07 +0900

    basic_menu.java modification

--- app/src/main/java/kr/hs/mirim/japan_gifu/basic_menu.java
+++ app/src/main/java/kr/hs/mirim/japan_gifu/basic_menu.java
@@ -1,13 +1,74 @@
 package kr.hs.mirim.japan_gifu;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class basic_menu extends AppCompatActivity {
+    Context context;
+
+    ImageButton search;
+
+    TextView menu_text;
+    RelativeLayout back;
+
+    String menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_menu);
+
+        Intent intent = getIntent();
+        menu = intent.getStringExtra("menu_type");
+
+        menu_text = findViewById(R.id.top_view_text);
+
+        switch (menu){
+            case "sightseeing":
+                menu_text.setText("SIGHTSEEING");
+                break;
+
+            case "stay":
+                menu_text.setText("STAY");
+                break;
+
+            case "food":
+                menu_text.setText("FOOD");
+                break;
+        }
+
+        search = (ImageButton)findViewById(R.id.btn_search);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, search.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+            }
+        });
+
+        back = (RelativeLayout) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                String activity = intent.getExtras().getString("activity");
+                if (activity.equals("category")) {
+                    intent = new Intent(context, category.class);
+                } else if(activity.equals("main")){
+                    intent = new Intent(context, MainActivity.class);
+                }
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+
+                finish();
+            }
+        });
     }
 }
