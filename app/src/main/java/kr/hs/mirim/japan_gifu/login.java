4900c20194744c26411a6f4ba4546b1d3b2711b1
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-06-10 23:13:40 +0900

    로그인 기능 구현 시작

--- app/src/main/java/kr/hs/mirim/japan_gifu/login.java
+++ app/src/main/java/kr/hs/mirim/japan_gifu/login.java
@@ -16,6 +16,8 @@ public class login extends AppCompatActivity {
     EditText E_email;
     EditText E_password;
 
+    String email, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +31,67 @@ public class login extends AppCompatActivity {
 
         login.setOnClickListener(gotoHome);
     }
+
+    /*
+    class check{
+            int idcheck=0,passwordcheck=0;
+        }
+            check c = new check();
+
+            @Override
+            public void onClick(View v) {
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                FirebaseDatabase mdatabase = FirebaseDatabase.getInstance();
+                DatabaseReference mdatabaseRef = mdatabase.getReference("id");
+
+                mdatabaseRef.addValueEventListener(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for(DataSnapshot messeage : dataSnapshot.getChildren()){
+                            if(messeage.getValue().toString().equals(loginid.getText().toString())) {
+                                c.idcheck=1;
+                            }
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+
+                FirebaseDatabase mdatabasepwd = FirebaseDatabase.getInstance();
+                DatabaseReference mdatabaseRefpwd = mdatabasepwd.getReference("user");
+
+                mdatabaseRefpwd.child(loginid.getText().toString()).child("passwd").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot messeage : dataSnapshot.getChildren()) {
+                            if (messeage.getValue().toString().equals(loginpwd.getText().toString())) {
+                                if (c.idcheck == 1) {
+                                    Toast.makeText(login.this, "로그인 완료!", Toast.LENGTH_SHORT).show();
+                                    c.passwordcheck=1;
+                                    Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+                                    intent.putExtra("id",loginid.getText().toString());
+                                    intent.putExtra("pwd",loginpwd.getText().toString());
+                                    startActivity(intent);
+                                }
+                            }
+                        }
+                        if(!(c.idcheck==1&&c.passwordcheck==1)){
+                            Toast.makeText(login.this, "아이디나 비밀번호가 잘못되었습니다. 다시 입력해주세요", Toast.LENGTH_SHORT).show();
+                            loginid.setText("");
+                            loginpwd.setText("");
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+            }
+        });
+     */
 
     Button.OnClickListener gotoHome = new View.OnClickListener() {
         @Override
