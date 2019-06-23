3298090ffc2b2b6f03ee0ba5ef3831d53c858a9a
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-06-24 03:38:22 +0900

    post layout 생성

---
+++ app/src/main/java/kr/hs/e_mirim/Japan_Gifugi/post.java
@@ -0,0 +1,15 @@
+package kr.hs.e_mirim.Japan_Gifugi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class post extends AppCompatActivity {
+
+
+    String E_name, period, time, holiday, price, tel, site;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
