bb792ae68c78269116dd19960596b61059d6c621
Eunjin Lee <s2017s15@e-mirim.hs.kr>
2019-06-25 14:00:17 +0900

    act list view add(front)

--- app/src/main/java/kr/hs/e_mirim/Japan_Gifugi/Actlist.java
+++ app/src/main/java/kr/hs/e_mirim/Japan_Gifugi/Actlist.java
@@ -3,11 +3,11 @@ package kr.hs.e_mirim.Japan_Gifugi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class category_list_act extends AppCompatActivity {
+public class Actlist extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category_list_act);
+        setContentView(R.layout.category_list_act);
     }
 }
